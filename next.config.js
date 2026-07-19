3a9e73be0b43347c2ab2b94ad5f4a11ea6fc48cd
Edgardo Hernandez <asiariko2019@gmail.com>
2026-07-19 14:56:10 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/Digital-Growth',
+  assetPrefix: '/Digital-Growth',
   images: {
     unoptimized: true,
   },
